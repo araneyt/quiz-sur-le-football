51f04675b049d34e182ec0cd3f5eb0741c44570c
araneyt <162423167+araneyt@users.noreply.github.com>
2024-04-29 20:54:06 +0200

    Delete pages directory

--- pages/page1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-title = st.text_input('Gib deinen Namen ein', 'Elena')
-st.write('Spielername', title)
-
-st.page_link("pages/page2.py", label="Weiter", icon="😸")
--- pages/page4.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Start")
-
-st.page_link("pages/page5.py", label="Weiter", icon="😸")

dd0e224c814a031b0d57dcf21cee8935591e76a4
araneyt <162423167+araneyt@users.noreply.github.com>
2024-04-29 22:36:11 +0200

    Update page5.py

--- pages/page5.py
+++ pages/page5.py
@@ -41,23 +41,15 @@ def main():
     # Anzeige einer zufälligen Frage
     question = show_question(questions)
 
-    # Timer von 10 herunterzählen für die Auswahl der Antwort
-    timer_text = st.empty()
-    for i in range(10, 0, -1):
-        timer_text.text(f"Timer: {i}")
-        time.sleep(1)
-
-    # Anzeige der Antwortmöglichkeiten
-    correct_answer = questions.loc[questions["question"] == question, "distractor1"].values[0]
-    selected_answer = show_answers(questions, correct_answer)
-    
     # Timer von 15 herunterzählen für die Anzeige der richtigen Antwort
-    timer_text.empty()
     timer_text = st.empty()
     for i in range(15, 0, -1):
         timer_text.text(f"Timer: {i}")
         time.sleep(1)
         if i == 1:
+            # Anzeige der Antwortmöglichkeiten
+            correct_answer = questions.loc[questions["question"] == question, "distractor1"].values[0]
+            selected_answer = show_answers(questions, correct_answer)
             st.write(f"Die richtige Antwort ist: {correct_answer}")
 
 if __name__ == "__main__":

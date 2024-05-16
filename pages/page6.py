8f6b5dd8a5a5d60c58fa4b3909090f2a63867dee
araneyt <162423167+araneyt@users.noreply.github.com>
2024-05-16 10:38:01 +0200

    Delete pages directory

--- pages/page6.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Lösung")
-
-st.text("„C'est en forgeant qu'on devient forgeron (Übung macht den Meister), Name")
-
-st.page_link("pages/page5.py", label="Weiter", icon="😸")

03d7ba2253bc5b65865cdc3bae7f8135b7a47bf0
araneyt <162423167+araneyt@users.noreply.github.com>
2024-05-22 10:00:23 +0200

    Add files via upload

---
+++ pages/page1.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+# Funktion zur Darstellung der Seite "Name eingeben"
+def page_enter_name():
+    st.title("Namen eingeben")
+    name = st.text_input("Bitte gib Deinen Namen ein und bestätige mit Enter:")
+    if name:
+        st.session_state.name = name
+        st.write(f"Willkommen, {name}!")
+        st.write("Klicke unten auf den Knopf, um das Spiel zu starten.")
+
+# Hauptprogramm
+def main():
+    # Initialisierung des Namens in der Session, falls nicht vorhanden
+    if "name" not in st.session_state:
+        st.session_state.name = ""
+    page_enter_name()
+    # Anzeige des Spiel starten Links, falls ein Name eingegeben wurde
+    if st.session_state.name:
+        st.page_link("pages/page2.py", label="Hier geht es weiter", icon="🪴")
+
+if __name__ == "__main__":
+    main()
---
+++ pages/page3.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import time
+
+def main():
+    # Zugriff auf den eingegebenen Namen von Seite 1
+    player_name = st.session_state.name
+    # Farbige Box mit dem Titel und dem Namen erstellen
+    st.markdown(
+        f'<div style="background-color:#c5eef0; padding:10px; border-radius:5px;">'
+        f'<h2 style="color:black; font-size:22px;">"Après la pluie, le beau temps." (Nach dem Regen kommt Sonnenschein 🌞):</h2>'
+        f'<span style="color:black; font-size:22px;"> {player_name}</span>'
+        '</div>',
+        unsafe_allow_html=True
+    )
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    st.title("Mach dich bereit!")
+
+    # Timer von 10 herunterzählen
+    timer_text = st.empty()
+    for i in range(5, 0, -1):
+        timer_text.text(f"Timer: {i}")
+        time.sleep(1)
+
+    # Ausblenden des Bildes im Hintergrund
+    st.markdown('<style>body {background-image: none}</style>', unsafe_allow_html=True)
+    #Verlinkung zur nächsten Seite mit einem Button
+    st.page_link("pages/page4.py", label="Los geht's", icon="🪴")
+
+if __name__ == "__main__":
+    main()
+
